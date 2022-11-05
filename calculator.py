40d0920085b75558b06bf71b96f80567ac152a9d
Gurchandan Singh <110492097+ChanndKaleka@users.noreply.github.com>
2022-11-05 14:29:27 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def calc(a,b,c):
+    print(eval(f"{a}{b}{c}"))
+    
+firstNumber=int(input("Enter the first number: "))
+operator=input("Enter the operator: ")
+secondNumber=input("Enter the second number: ")
+
+calc(firstNumber,operator,secondNumber)
